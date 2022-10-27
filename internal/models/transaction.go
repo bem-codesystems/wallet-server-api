c04145276d4959d63c55a3dd0954dc725712b3a0
AdrianoHeller <adriano@technoizz.com>
2022-10-26 23:40:41 -0300

    feat: add transaction creator

--- internal/models/transaction.go
+++ internal/models/transaction.go
@@ -1,14 +1,37 @@
 package models
 
-import "database/sql"
+import (
+	"database/sql"
+	"time"
+	"wallet-server/helpers"
+)
 
 type Transaction struct {
-	Id              string  `json:"id"`
-	TransactionType uint    `json:"transaction_type"`
-	Value           float32 `json:"value"`
-	WalletID        string  `json:"wallet_id"`
+	ID              string    `json:"id"`
+	TransactionType uint      `json:"transaction_type"`
+	Value           float32   `json:"value"`
+	CreatedAt       time.Time `json:"created_at"`
+	WalletID        string    `json:"wallet_id"`
 }
 
 type TransactionModel struct {
 	DB *sql.DB
+}
+
+func (t *TransactionModel) Create(tp uint, value float32, walletID string) (int, error) {
+	randomID, err := helpers.CreateRandomID(100)
+	if err != nil {
+		return 0, err
+	}
+	stmt := `INSERT INTO transactions(id,transaction_type,value,created_at,wallet_id)VALUES(?,?,?,UTC_TIMESTAMP(),?)`
+
+	res, err := t.DB.Exec(stmt, randomID, tp, value, walletID)
+	if err != nil {
+		return 0, err
+	}
+	id, err := res.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+	return int(id), nil
 }
