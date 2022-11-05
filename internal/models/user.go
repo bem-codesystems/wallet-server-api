cfb5adc10fe75d746aab267ee825e32209abc17b
AdrianoHeller <adriano@technoizz.com>
2022-11-04 21:23:57 -0300

    feat: add user model

---
+++ internal/models/user.go
@@ -0,0 +1,48 @@
+package models
+
+import (
+	"database/sql"
+	"time"
+	"wallet-server/helpers"
+)
+
+type User struct {
+	ID        string    `json:"id"`
+	HasWallet bool      `json:"has_wallet"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	WalletID  string    `json:"wallet_id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type UserModel struct {
+	DB *sql.DB
+}
+
+func (u *UserModel) Create(hasWallet bool, name string, email string) (int, error) {
+	stmt := `INSERT INTO users (
+                   id,
+                   has_wallet,
+                   name,
+                   email,
+                   wallet_id,
+                   created_at,
+                   updated_at) VALUES (?,?,?,?,?,UTC_TIMESTAMP(),UTC_TIMESTAMP())`
+
+	hash, err := helpers.CreateRandomID(50)
+	if err != nil {
+		return 0, err
+	}
+
+	res, err := u.DB.Exec(stmt, hash, false, name, email)
+	if err != nil {
+		return 0, err
+	}
+
+	id, err := res.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+	return int(id), err
+}
